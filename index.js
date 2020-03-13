31861da1e895566382ec61f5c8a6bf2b718a55ff
Kc Kai <kirstenckai@gmail.com>
2020-03-13 17:35:58 +0000

    Refactor index.js to be object oriented

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+// Object with task, desciption and complete states
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+    // this refers to the state outside the function
+    // Print the state of a task to the console in a nice readable way
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // Mark a task as complete by setting the task's status to true
+    markCompleted: function() {
+      this.complete = true;
+    },
+  };
+
+  return task;
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+// for now, let's just make sure we see our tasks
+task1.logState(); // passes task1 to logState function
+task1.markCompleted(); // passes task1 to markCompleted function, which then marks the task as completed to be true
+task1.logState(); // passes the newly completed task to logState function which then says that the task is complete
+
